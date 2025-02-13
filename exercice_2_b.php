8a65cf436f5a6ac8b3a6fcbed9cd22a2b836a0c0
ridah00 <mahdjoubridah00@gmail.com>
2025-02-13 14:15:15 +0100

    exercice 2 b

---
+++ exercice_2_b.php
@@ -0,0 +1,12 @@
+
+<?php 
+$isEasy = true ;
+echo ($isEasy) ? "c'est facile." : "c'est difiicile.";
+$isEasy = false ;
+echo '<br>';
+if ($isEasy) {
+    echo "c'est facile.";
+}else {
+   echo "c'est dificile.";
+}
+?>
